11c5917c5fe6394f87fb18d5a9a6b1ab733595a9
diproch7 <34616184+diproch7@users.noreply.github.com>
2020-04-28 11:49:52 +0530

    Update models.js

--- models.js
+++ models.js
@@ -1,11 +1,34 @@
-const mongoose = require('./node_modules/mongoose');
-
+const mongoose = require('mongoose');
 
 const userschema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
     },
+
+    test: {
+      type: Number,
+	  default: 0,
+    },
+	remaining: {
+		type: Number,
+		default: 40,
+	},
+	max: {
+        type: Number,
+        default: 0,
+    },
+	min: {
+        type: Number,
+        default: 0,
+    },
+	
+	
+	
+	name: {
+		type:String,
+	},
+
     username: {
         type: String,
         required: true,
@@ -14,6 +37,26 @@ const userschema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    admin:{
+      type: Number,
+    },
+	
+	dob:{
+		type: String,
+	},
+	
+	year:{
+		type: Number,
+	},
+	
+	phone:{
+		type: String,
+	},
+	
+	university:{
+		type: String,
+	},
+	
     messages: [{
         type: String,
     }]
