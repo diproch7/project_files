9903cc2b2c19a729749f539afed256dd05187bf9
diproch7 <34616184+diproch7@users.noreply.github.com>
2020-04-28 11:51:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const routes = require('./routes.js');
 const path = require('path');
 app.use(express.static("public"));
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
@@ -20,9 +14,17 @@ app.post('/login', routes);
 app.get('/success', routes);
 app.get('/logout', routes);
 app.get('/examcenter', routes);
-app.get('/upload',routes);
 app.post('/addmsg', routes);
+app.get('/upload',routes);
 app.post('/add_question',routes);
-
+app.get('/exam_form',routes);
+app.post('/exam_form',routes);
+app.get('/exam_page',routes);
+app.post('/exam_page',routes);
+app.get('/result',routes);
+app.get('/forbidden',routes);
+app.get('/dashboard',routes);
+app.post('/dashboard',routes);
+app.get('/answerKey',routes);
 const PORT = 3000;
 app.listen(PORT, () => console.log("Server Started At Port", PORT));
